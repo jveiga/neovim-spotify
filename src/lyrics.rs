7977b2b91b0538587e88e2f1b2bbfd5f737c7ec4
Joao Veiga <joao@fishbrain.com>
2020-01-10 13:07:06 +0100

    make find_lyrics async

--- src/lyrics.rs
+++ src/lyrics.rs
@@ -1,9 +1,6 @@
-extern crate reqwest;
-extern crate soup;
-
 use soup::prelude::*;
 
-pub fn find_lyrics(artist: &str, song: &str) -> Option<String> {
+pub async fn find_lyrics(artist: &str, song: &str) -> Option<String> {
     let artist = artist.split_whitespace().collect::<Vec<_>>().join("-");
     let song = song.split_whitespace().collect::<Vec<_>>().join("-");
     let ident = format!("{}-{}-lyrics", artist, song);
@@ -20,17 +17,4 @@ pub fn find_lyrics(artist: &str, song: &str) -> Option<String> {
     let lyrics = format!("{} - {}\n {}", artist, song, lyrics);
 
     Some(lyrics.to_owned())
-}
-
-#[cfg(test)]
-use super::*;
-
-#[test]
-fn it_works() {
-    assert_eq!(find_lyrics("Tesseract", "Seven Names").is_some(), true);
-    assert_eq!(
-        find_lyrics("Led Zeppelin", "Babe I'm Gonna Leave You").is_some(),
-        true
-    );
-    assert_eq!(find_lyrics("X", "Y").is_none(), true);
 }
